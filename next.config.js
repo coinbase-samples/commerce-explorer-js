24ccc67f638dd84c709a198fd005e43575a5d78f
Patrick Hughes <patrick.hughes@coinbase.com>
2022-12-08 18:19:34 -0800

    adding environment variables

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+
+const withTranspileModules = require("next-transpile-modules");
+const withPlugins = require("next-compose-plugins");
+const LicensePlugin = require("webpack-license-plugin");
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    //front end method of retreiving env variables
+    API_KEY: "YOUR_API_KEY", //NEXT_PUBLIC_API_KEY
+    API_VERSION: "2018-03-22", //NEXT_PUBLIC_API_VERSION
+  },
+};
+
+module.exports = withPlugins([
+  nextConfig,
+  withTranspileModules(["@cloudscape-design/components"]),
+  new LicensePlugin({ outputFilename: "thirdPartyNotice.json" }),
+]);
