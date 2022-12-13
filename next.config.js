d7252e2c51407e0822620870e99b82d5ba002910
Carl Fluke <carl.fluke@coinbase.com>
2022-12-13 08:57:25 -0500

    add different license gen library

--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const withTranspileModules = require("next-transpile-modules");
-const withPlugins = require("next-compose-plugins");
-const LicensePlugin = require("webpack-license-plugin");
+const withTranspileModules = require('next-transpile-modules');
+const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
     //front end method of retreiving env variables
-    API_KEY: "YOUR_API_KEY", //NEXT_PUBLIC_API_KEY
-    API_VERSION: "2018-03-22", //NEXT_PUBLIC_API_VERSION
+    API_KEY: 'YOUR_API_KEY', //NEXT_PUBLIC_API_KEY
+    API_VERSION: '2018-03-22', //NEXT_PUBLIC_API_VERSION
   },
 };
 
 module.exports = withPlugins([
   nextConfig,
-  withTranspileModules(["@cloudscape-design/components"]),
-  new LicensePlugin({ outputFilename: "thirdPartyNotice.json" }),
+  withTranspileModules(['@cloudscape-design/components']),
 ]);
